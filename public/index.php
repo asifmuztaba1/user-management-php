8dc7db72a5cac88b50ff8fe2005315481817d113
asifmuztaba1 <amuztaba18@gmail.com>
2024-06-08 17:29:06 +0600

    Added default index file

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
-phpinfo();
 
+require_once __DIR__ . '/../vendor/autoload.php'; // Adjust the path to autoload.php as necessary
+
+use Asifmuztaba\UserManagement\Routes\Web;
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/../.env');
+$router = new Web();
+$router->handle();
