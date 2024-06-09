6393000241e253548758b4140eb51cca675a628b
asifmuztaba1 <amuztaba18@gmail.com>
2024-06-09 22:57:10 +0600

    added service container

--- public/index.php
+++ public/index.php
@@ -2,10 +2,23 @@
 
 require_once __DIR__ . '/../vendor/autoload.php'; // Adjust the path to autoload.php as necessary
 
+use Asifmuztaba\UserManagement\Managers\ContainerManager;
+use Asifmuztaba\UserManagement\Managers\DatabaseManager;
+use Asifmuztaba\UserManagement\Models\User;
 use Asifmuztaba\UserManagement\Routes\Web;
 use Symfony\Component\Dotenv\Dotenv;
 
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/../.env');
-$router = new Web();
+$container = new ContainerManager();
+
+$container->singleton(DatabaseManager::class, function () {
+    return new DatabaseManager();
+});
+
+$container->bind(User::class, function ($container) {
+    return new User($container->resolve(DatabaseManager::class));
+});
+
+$router = new Web($container);
 $router->handle();
